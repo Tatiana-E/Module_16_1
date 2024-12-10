d140cc075e590bb5061a77f50b6751d7f07e9fe3
Tatiana-E <tanya.era1989@gmail.com>
2024-12-10 16:07:11 +0500

    Add files via upload

---
+++ Module_16_1.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+
+# Создаем экземпляр приложения FastAPI
+app = FastAPI()
+# Определение базового маршрута
+
+@app.get("/user/admin")
+async def admin_root():
+    return {"message": "Вы вошли как администратор"}
+
+@app.get("/user/{user_id}")
+async def user_root(user_id: int):
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
+
+@app.get("/user")
+async def user_root(username: str, age: int):
+    return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
+
+@app.get("/")
+async def root():
+    return {"message": "Главная страница"}
+
