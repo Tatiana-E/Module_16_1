59d42a41296b48bf836a7e7db6fd29e6d5017cc4
Tatiana-E <tanya.era1989@gmail.com>
2024-12-11 18:43:16 +0500

    Update Module_16_1.py

--- Module_16_1.py
+++ Module_16_1.py
@@ -10,11 +10,11 @@ async def admin_root():
     return {"message": "Вы вошли как администратор"}
 
 @app.get("/user/{user_id}")
-async def user_root(user_id: int):
+async def user_root(user_id: int)->str:
     return {"message": f"Вы вошли как пользователь № {user_id}"}
 
 @app.get("/user")
-async def user_root(username: str, age: int):
+async def user_root(username: str, age: int) ->str:
     return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
 
 @app.get("/")
